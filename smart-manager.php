bda4e43f60214a60ba664ccbb9822a06e0960b56
niravmehta <niravmehta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-02 16:49:09 +0000

    Changes for 3.1.2
    git-svn-id: https://plugins.svn.wordpress.org/smart-manager-for-wp-e-commerce/trunk@690705 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smart-manager.php
+++ smart-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: Smart Manager for e-Commerce
 Plugin URI: http://www.storeapps.org/product/smart-manager/
 Description: <strong>Lite Version Installed</strong> 10x productivity gains with WP e-Commerce & WooCommerce store administration. Quickly find and update products, variations, orders and customers.
-Version: 3.1.1
+Version: 3.1.2
 Author: Store Apps
 Author URI: http://www.storeapps.org/
 Copyright (c) 2010, 2011, 2012, 2013 Store Apps All rights reserved.
